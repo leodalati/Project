ded2c7a2ff66da796bf732ff28e0613db1b83334
Leo Dalati <leo.dalati@ontariotechu.net>
2025-11-13 14:12:19 -0500

    Added navbar and ejs files for CRUD

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+/* GET Create page. */
+router.get('/create', function(req, res, next) {
+  res.render('create', { title: 'Create' });
+});
+/* GET Update page. */
+router.get('/update', function(req, res, next) {
+  res.render('update', { title: 'Update' });
+});
+/* GET Delete page. */
+router.get('/delete', function(req, res, next) {
+  res.render('delete', { title: 'Delete' });
 });
 
 module.exports = router;
